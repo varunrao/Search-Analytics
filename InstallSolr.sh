7928fab6f0f59b54b259cf7ff86bc6c403353b17
Varun Rao <varunraob@gmail.com>
2014-09-01 12:09:29 -0500

    Files to install search analytics

---
+++ InstallSolr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ~
+wget http://archive.apache.org/dist/lucene/solr/4.7.2/solr-4.7.2.tgz
+tar -xvf solr-4.7.2.tgz
+cd ~
+git clone https://github.com/LucidWorks/banana.git
+cd banana
+sed -i 's/localhost/'"`hostname -i`"'/' src/config.js
+sed -i 's/localhost/'"`hostname -i`"'/' src/app/dashboards/default.json
+rm -rf build
+mkdir build
+ant
+ cp build/banana*.war ~/solr-4.7.2/example/webapps/banana.war
+cp jetty-contexts/banana-context.xml ~/solr-4.7.2/example/contexts/
+cd ~/solr-4.7.2/example/
+ java -jar start.jar &
