8a98452a63c1c565beb2a9e1f46dcc9daefb25fa
Varun Rao <varunraob@gmail.com>
2014-09-01 21:14:49 -0500

    Updates

--- InstallApplication.sh
+++ InstallApplication.sh
@@ -12,14 +12,13 @@ export M2_HOME=~/apache-maven/apache-maven-3.2.1
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 # Install app code
-cd ~
-cd Search-Analytics
+cd ~/Search-Analytics
 ## Install flume hbase serializer
-cd hbase-serializer
+cd ~/Search-Analytics/hbase-serializer
 mvn clean package
 cp target/hbase-serializer-0.0.1-SNAPSHOT.jar /var/lib/flume/lib/
 ## Install flume custom Twitter4j Source
-cd flume-sources
+cd ~/Search-Analytics/flume-sources
 ## Replace twitter variables
 sed -i "s/consumer_Key/$consumer_Key/" flume.conf
 sed -i "s/consumer_Secret/$consumer_Secret/" flume.conf
@@ -29,6 +28,6 @@ mvn clean package
 cp flume.conf /etc/flume/conf/
 
 ## Install hbase solr indexer
-cd hbase-indexer
-mvn clean package -DskipTests=true
+cd ~/Search-Analytihbase-indexer
+mvn package -DskipTests=true
 bin/hbase-indexer server
